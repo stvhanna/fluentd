d81ef4333eaa2de042da2d9011d50cdae0f0a1bc
TAGOMORI Satoshi <tagomoris@gmail.com>
2016-12-20 19:10:38 +0900

    writing

--- lib/fluent/command/fluentd.rb
+++ lib/fluent/command/fluentd.rb
@@ -69,6 +69,10 @@ op.on('--no-supervisor', "run fluent worker without supervisor") {
   opts[:standalone_worker] = true
 }
 
+op.on('--workers NUM', "specify the number of workers under supervisor") { |i|
+  opts[:workers] = i.to_i
+}
+
 op.on('--user USER', "change user") {|s|
   opts[:chuser] = s
 }
@@ -285,5 +289,9 @@ require 'fluent/supervisor'
 if opts[:supervise]
   Fluent::Supervisor.new(opts).run_supervisor
 else
+  if opts[:workers] > 1
+    puts "Error: multi workers is not supported with --no-supervisor"
+    exit 1
+  end
   Fluent::Supervisor.new(opts).run_worker
 end
